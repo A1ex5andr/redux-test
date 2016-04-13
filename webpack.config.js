e1c0b83bd9606c56adde7bc3c223b02329b61080
Coderbit <sashayu@me.com>
2016-04-13 17:18:45 +0300

    hot loader init

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,32 @@
+var path = require('path');
+var webpack = require('webpack');
+
 module.exports = {
+  devtool: 'eval',
   entry: [
-    './src/index.js'
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    // './src/index.js'
+    './src/index'
   ],
   output: {
-    path: __dirname,
+    // path: __dirname,
+    path: path.join(__dirname, 'dist'),
     publicPath: '/',
     filename: 'bundle.js'
   },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
   module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel'
-    }]
+    loaders: [
+      {
+        exclude: /node_modules/,
+        test: /\.jsx?$/,
+        loaders: ['react-hot', 'babel'],
+        include: path.join(__dirname, 'src')
+      }
+    ]
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
